30cfb18441c4b8726a07979f682f615e8b519dd9
HOANGTSUPTONJV250825 <phamvanhoang2105@gmail.com>
2025-10-11 21:07:28 +0900

    upload bai tap 13.1

---
+++ bai tap 13.1/bai tap 13.1.js
@@ -0,0 +1,11 @@
+let menu = [
+    "banh bot loc",
+    "banh bao",
+    "banh beo",
+    "banh bong lan",
+    "banh khot"
+];
+menu.push("banh canh");
+menu.unshift("banh bong lan");
+menu.splice(1,0,"banh canh ca loc","banh mi", "banh bao chien");
+console.log(menu);
