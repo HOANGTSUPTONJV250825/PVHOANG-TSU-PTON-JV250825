6f935d8fa6ee776ab16f0a83616b42542ba1baee
HOANGTSUPTONJV250825 <phamvanhoang2105@gmail.com>
2025-10-16 21:38:45 +0900

    Add files via upload

---
+++ bai tap17.4/bai tap 17.4.js
@@ -0,0 +1,12 @@
+let ov1 = document.getElementById('ov1');
+let ov2 = document.getElementById('ov2');
+let ov3 = document.getElementById('ov3');
+ov1.addEventListener('mousemove',function(){
+    document.body.style.backgroundColor = 'yellow';
+});
+ov2.addEventListener('mousemove',function(){
+    document.body.style.backgroundColor = 'green';
+});
+ov3.addEventListener('mousemove',function(){
+    document.body.style.backgroundColor = 'gray';
+});
