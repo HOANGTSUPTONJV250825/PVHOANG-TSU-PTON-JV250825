eba68dd9b915c9ee13d6f7f65b9c09f8b92c31ac
HOANGTSUPTONJV250825 <phamvanhoang2105@gmail.com>
2025-11-20 13:24:30 +0900

    upload bai tap 2.4.1

---
+++ bai tap 2.4.1/bai tap 2.4.1.sql
@@ -0,0 +1,20 @@
+USE companyDB;
+
+CREATE TABLE Students (
+StudentID INT PRIMARY KEY,
+Name VARCHAR(100),
+Age INT,
+Major VARCHAR(100)
+);
+
+INSERT INTO Students(StudentID, Name, Age, Major)
+VALUE 
+(1, 'Alice', 20, 'Computer Science'),
+(2, 'Bob', 22, 'Mathematics'),
+(3, 'Charlie', 21, 'Physics');
+
+SELECT *
+FROM Students;
+
+
+
