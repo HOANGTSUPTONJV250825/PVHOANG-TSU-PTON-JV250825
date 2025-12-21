986fcb713720702c392610d5bf4058884463fd18
HOANGTSUPTONJV250825 <phamvanhoang2105@gmail.com>
2025-12-21 10:41:27 +0900

    upload bai tap 3.1.4

---
+++ Exercise04.java
@@ -0,0 +1,14 @@
+package exercise04;
+
+public class Exercise04 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 5;
+        System.out.println("a = 10 va b = 5");
+        System.out.println("tong cua a + b la:"+(a + b));
+        System.out.println("hieu cua a - b la:"+(a - b));
+        System.out.println("tich cua a * b la:"+(a * b));
+        System.out.println("thuong cua a / b la:"+(a / b));
+        System.out.println("so du khi chia a cho b la:"+(a % b));
+    }
+}
