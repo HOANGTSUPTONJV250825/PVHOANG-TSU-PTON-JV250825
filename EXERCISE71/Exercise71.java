85c0f9fbe734aea4ad03bbe04cfdd1fd24c60671
HOANGTSUPTONJV250825 <phamvanhoang2105@gmail.com>
2026-01-05 15:03:55 +0900

    upload bia tap 3.7.1

---
+++ EXERCISE71/Exercise71.java
@@ -0,0 +1,13 @@
+package EXERCISE71;
+
+public class Exercise71 {
+    public static void main(String[] args) {
+        Rectangle rec1 = new Rectangle();
+        System.out.println("hinh chu nhat 1: ");
+        rec1.display();
+
+        Rectangle rec2 = new Rectangle(5.0,3.0);
+        System.out.println("\nhinh chu nhat 2: ");
+        rec2.display();
+    }
+}
