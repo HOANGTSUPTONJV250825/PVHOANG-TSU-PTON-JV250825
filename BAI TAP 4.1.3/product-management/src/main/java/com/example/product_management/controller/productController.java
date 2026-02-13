9c19a904f5e4aebf4b8300ccecf891ecf5c12f4d
HOANGTSUPTONJV250825 <phamvanhoang2105@gmail.com>
2026-02-13 10:10:56 +0900

    upload bai tap 4.1.4

--- BAI TAP 4.1.3/product-management/src/main/java/com/example/product_management/controller/productController.java
+++ BAI TAP 4.1.3/product-management/src/main/java/com/example/product_management/controller/productController.java
@@ -1,11 +1,22 @@
 package com.example.product_management.controller;
 
+import com.example.product_management.model.products;
 import com.example.product_management.sevice.productSevice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/products")
 public class productController {
     @Autowired
     private productSevice productSevice;
+
+    @GetMapping
+    public List<products> getProducts(){
+        return productSevice.getAllProducts();
+    }
 }
